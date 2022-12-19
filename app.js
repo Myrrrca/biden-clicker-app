90ebe9bb3d353e74875a58cced3da8a03f84de4a
myrrrca <damskieiygodnik217@gmail.com>
2022-12-19 23:24:06 +0300

    5

--- app.js
+++ app.js
@@ -3,21 +3,29 @@ const scoreText = document.querySelector("#score-counter");
 
 let score = 0;
 
-const clickOnBiden = () => {
-  ++score;
-  updateScoreCounter();
-  changeBidenFace();
-};
-
 const updateScoreCounter = () => {
   scoreText.innerHTML = `${score}`;
 };
 
 const changeBidenFace = () => {
-  bidenImg.src = "pics/biden-bonk.png";
+  bidenImg.src = "pics/biden-bonk-tears.png";
   setTimeout(() => {
     bidenImg.src = "pics/biden-based.png";
   }, 200);
+};
+
+const changeCursorBonk = (event) => {
+  event.target.classList.add("bonk_cursor");
+  setTimeout(() => {
+    event.target.classList.remove("bonk_cursor");
+  }, 200);
+};
+
+const clickOnBiden = (event) => {
+  ++score;
+  updateScoreCounter();
+  changeBidenFace();
+  changeCursorBonk(event);
 };
 
 bidenImg.addEventListener("click", clickOnBiden);
