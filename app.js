06ef286bcb645f810b82573f8607e48437a77519
myrrrca <damskieiygodnik217@gmail.com>
2022-12-21 00:40:51 +0300

    version 2

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const bidenImg = document.querySelector("#biden");
 const scoreText = document.querySelector("#score-counter");
+const buttonTrump = document.querySelector(".button-tramp");
+const trumpsSpace = document.querySelector(".trumps");
 
 let score = 0;
+let trumpCounter = 0;
 
 const updateScoreCounter = () => {
   scoreText.innerHTML = `${score}`;
@@ -21,11 +24,57 @@ const changeCursorBonk = (event) => {
   }, 200);
 };
 
+const updateTrumpButton = () => {
+  if (score >= 50) {
+    buttonTrump.classList.add("active-button");
+  }
+};
+
 const clickOnBiden = (event) => {
   ++score;
   updateScoreCounter();
   changeBidenFace();
   changeCursorBonk(event);
+  updateTrumpButton();
+};
+
+const addTrump = () => {
+  if (trumpCounter >= 6 && buttonTrump.classList.contains("active-button")) {
+    updateTrumpButton();
+    return alert("There is max amount of Trumps");
+  }
+  if (buttonTrump.classList.contains("active-button")) {
+    let img = document.createElement("img");
+    img.src = "./pics/trump.jpg";
+    img.style.display = "flex";
+    img.style.justifyContent = "center";
+    img.style.alignItems = "center";
+    trumpsSpace.append(img);
+    updateTrumpButton();
+    ++trumpCounter;
+    console.log(trumpCounter);
+  }
+};
+
+const clickOnTrumpButton = (event) => {
+  if (buttonTrump.classList.contains("active-button") && !(trumpCounter > 6)) {
+    updateScoreCounter();
+    addTrump();
+    buttonTrump.classList.remove("active-button");
+    updateTrumpButton();
+    if (!(trumpCounter > 5)) {
+      score -= 50;
+    }
+  }
 };
 
 bidenImg.addEventListener("click", clickOnBiden);
+buttonTrump.addEventListener("click", clickOnTrumpButton);
+
+const preapareGame = () => {
+  score = 488;
+  updateScoreCounter();
+  updateTrumpButton();
+};
+
+preapareGame();
